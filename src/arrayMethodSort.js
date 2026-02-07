39c3e76dca8f90f5b0fea64202ff2f590435bac3
Taras Peteichuk <taraskull123@gmail.com>
2026-02-07 20:13:57 +0200

    removed duplicated code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,21 +5,11 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    if (typeof compareFunction !== 'function') {
-      for (let i = 0; i < this.length - 1; i++) {
-        for (let j = 0; j < this.length - 1 - i; j++) {
-          if (String(this[j]) > String(this[j + 1])) {
-            [this[j], this[j + 1]] = [this[j + 1], this[j]];
-          }
-        }
-      }
-
-      return this;
-    }
+    let defaultCompare = compareFunction || ((a, b) => String(a) > String(b));
 
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - 1 - i; j++) {
-        if (compareFunction(this[j], this[j + 1]) > 0) {
+        if (defaultCompare(this[j], this[j + 1]) > 0) {
           [this[j], this[j + 1]] = [this[j + 1], this[j]];
         }
       }
